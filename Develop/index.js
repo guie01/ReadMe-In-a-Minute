4c603170ea166db53a03823bfd7e169c3a6885ec
Guillermo Garcia <ggq0107@hotmail.com>
2021-02-21 22:55:25 -0500

    added logic to create a readMe completely

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,100 @@
-// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project/application?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Enter a description of your project or application.',
+      name: 'description',
+      
+    },
+    {
+        type: 'input',
+        message: 'Enter installation instructions',
+        name: 'installation',
+      },
+      {
+        type: 'input',
+        message: 'Enter usage information',
+        name: 'usage',
+      },
+      {
+        type: 'input',
+        message: 'Enter contribution guidelines',
+        name: 'contribution',
+      },
+      {
+        type: 'input',
+        message: 'Enter test intructions',
+        name: 'test',
+      },
+      {
+        type: 'input',
+        message: 'Enter your GitHub username',
+        name: 'github',
+      },
+      {
+        type: 'input',
+        message: 'Enter your email address',
+        name: 'email',
+      },
+  ])
+  .then((data) => {
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    let readMeString = `# ${data.title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Table of Contents
+1. [ Application Description. ](#application)
+2. [ Installation. ](#installation)
+2. [ Usage. ](#usage)
+3. [ Contributing. ](#contribution)
+4. [ Test. ](#test)
+5. [ Questions. ](#questions)
+6. [ The other stuff: License & Badges. ](#license)
 
-// Function call to initialize app
-init();
+
+<a name="application"></a>
+## Application Description.
+
+${data.description}.
+<a name="installation"></a>
+## Installation.
+
+${data.installation}.
+
+<a name="usage"></a>
+## Usage
+
+${data.usage}.
+
+<a name="contribution"></a>
+## Contributing
+
+${data.contribution}.
+
+<a name="test"></a>
+## Test
+
+${data.test}.
+
+<a name="questions"></a>
+## Questions
+
+If you have any questions, feel free to contact at ${data.email}. Don't forget to check my GitHub profile: ${data.github}.
+
+<a name="license"></a>
+## Licenses & Badges
+
+.
+`
+
+    fs.writeFile("README.md", readMeString, (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
